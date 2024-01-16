597436c56df564cd2600ccd7ff90077a977d8f17
jwspdev <63495554+jwspdev@users.noreply.github.com>
2024-01-16 17:25:06 +0800

    UI Update: cards and container
    - Fixed overall ui
    - Implemented Card design
    - Implemented Pile of Card design

--- magic_app/lib/src/presentation/widgets/playing_card_ui.dart
+++ magic_app/lib/src/presentation/widgets/playing_card_ui.dart
@@ -13,41 +13,63 @@ class PlayingCardUi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 64,
+      height: 100,
       width: 64,
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
-          color: Colors.grey.shade200,
+          color: Colors.grey.shade100,
+          border: Border.all(color: Colors.grey.shade800),
+          // boxShadow: [
+          //   BoxShadow(
+          //     color: Colors.grey,
+          //     offset: const Offset(
+          //       1.0,
+          //       0.0,
+          //     ),
+          //     blurRadius: 1.0,
+          //     spreadRadius: 1.0,
+          //   )
+          // ]
         ),
         padding: EdgeInsets.all(8),
-        child: Column(
+        child: Stack(
           children: [
-            SizedBox(
-              width: 16,
-              height: 16,
-              child: Icon(_buildIcon()),
+            Positioned(
+                left: _checkLeftLength(), top: -5, child: Text('$value')),
+            Positioned(
+              // left: 1,
+              top: 15,
+              child: SizedBox(
+                width: 16,
+                height: 16,
+                child: Icon(_buildIcon()),
+              ),
             ),
-            SizedBox(
-              height: 8,
-            ),
-            Text('$value')
           ],
         ),
       ),
     );
   }
 
+  double _checkLeftLength() {
+    double val = double.tryParse(value) ?? 0.0;
+    if (val >= 10) {
+      return 0;
+    }
+    return 5;
+  }
+
   _buildIcon() {
     switch (suit) {
       case 'diamond':
-        return CupertinoIcons.suit_diamond;
+        return CupertinoIcons.suit_diamond_fill;
       case 'clover':
-        return CupertinoIcons.suit_club;
+        return CupertinoIcons.suit_club_fill;
       case 'spade':
-        return CupertinoIcons.suit_spade;
+        return CupertinoIcons.suit_spade_fill;
       case 'heart':
-        return CupertinoIcons.suit_heart;
+        return CupertinoIcons.suit_heart_fill;
     }
   }
 }
--- magic_app/lib/src/presentation/widgets/poker_table.dart
+++ magic_app/lib/src/presentation/widgets/poker_table.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:magic_app/src/data/models/playing_card.dart';
 import 'package:magic_app/src/presentation/bloc/magic_algorithm_bloc.dart';
+import 'package:magic_app/src/presentation/widgets/card_container.dart';
 import 'package:magic_app/src/presentation/widgets/custom_button.dart';
 import 'package:magic_app/src/presentation/widgets/playing_card_ui.dart';
 
@@ -21,51 +22,43 @@ class PokerTable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var magicAlgoBloc = context.read<MagicAlgorithmBloc>();
-    return Container(
-      padding: const EdgeInsets.all(8),
-      child: Column(
-        children: [
-          CustomButton(
-              text: 'Shuffle',
-              onPressed: () {
-                magicAlgoBloc.add(ShuffleCardsEvent());
-              }),
-          Expanded(
-            child: ListView.builder(
-              itemCount: mappedCard.length,
-              padding: EdgeInsets.all(8),
-              itemBuilder: (BuildContext context, int index) {
-                int key = mappedCard.keys.elementAt(index);
-                return SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: GestureDetector(
-                    onTap: () {
-                      magicAlgoBloc.add(SelectPileOfCardsEvent(key));
-                    },
-                    child: Container(
-                      color: Colors.green.shade300,
-                      margin: EdgeInsets.all(8),
-                      child: Column(
-                        children: mappedCard[key]!.map((card) {
-                          return SizedBox(
-                            width: 64,
-                            child: Container(
-                              padding: EdgeInsets.all(8),
-                              child: PlayingCardUi(
-                                value: card.value,
-                                suit: card.type,
-                              ),
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                  ),
-                );
-              },
+    return SafeArea(
+      child: Container(
+        decoration: BoxDecoration(color: Colors.green.shade800),
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 12),
+              child: CustomButton(
+                  text: 'Shuffle',
+                  onPressed: () {
+                    magicAlgoBloc.add(ShuffleCardsEvent());
+                  }),
             ),
-          ),
-        ],
+            Expanded(
+              child: ListView.builder(
+                itemCount: mappedCard.length,
+                padding: EdgeInsets.all(8),
+                itemBuilder: (BuildContext context, int index) {
+                  int key = mappedCard.keys.elementAt(index);
+                  return SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                          margin: EdgeInsets.all(8),
+                          child: CardContainer(
+                            cardList: mappedCard[key]!,
+                            pileIndex: key,
+                          )),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
